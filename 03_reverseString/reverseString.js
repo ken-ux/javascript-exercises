20beee71098f00a737b610b0fdf7408380971cd2
Kenny Nguyen <sokenns@gmail.com>
2023-10-23 01:07:22 -0700

    Add solution for third assessment

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversedString = "";
+    for (i = string.length - 1; i > -1; i--) {
+        reversedString += string[i];
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
