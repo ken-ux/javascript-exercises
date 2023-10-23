4e986fb7ef460298a41ff748d3ad3ee5a34255e0
Kenny Nguyen <sokenns@gmail.com>
2023-10-23 01:28:16 -0700

    Add solution for fourth assessment

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arrayToFilter) {
+    let filteredArray = arrayToFilter;
+    for (i = 0; i < arguments.length; i++) {
+        filteredArray = filteredArray.filter(item => item !== arguments[i]);
+    }
+    return filteredArray;
 };
 
 // Do not edit below this line
