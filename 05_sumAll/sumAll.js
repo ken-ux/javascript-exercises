36bf9363269ac4ce4516e707b07642987b01d4f5
Kenny Nguyen <sokenns@gmail.com>
2023-10-23 01:47:03 -0700

    Add solution to fifth assessment

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
+    let sum = 0;
+    let smallNum;
+    let bigNum;
 
+    if (typeof numOne !== "number" || typeof numTwo !== "number") {
+        return "ERROR";
+    }
+    
+    // Put inputs in the correct order for the for loop
+    if (numOne < numTwo) {
+        smallNum = numOne;
+        bigNum = numTwo;
+    } else {
+        smallNum = numTwo;
+        bigNum = numOne;
+    }
+
+    for (let i = smallNum; i <= bigNum; i++) {
+        sum += i;
+    }
+
+    if (sum < 0) {
+        return "ERROR";
+    } else {
+        return sum;
+    }
 };
 
 // Do not edit below this line
