1b0fe2790387ec2e743118c7dfc3ebeff7c5ef8e
Kenny Nguyen <sokenns@gmail.com>
2023-10-26 01:36:21 -0700

    Calculate age if person does has no yearOfDeath

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,14 +1,24 @@
 const findTheOldest = function (arr) {
+  // Add age to person properties
   for (let person in arr) {
     let personObject = arr[person];
     if (personObject.yearOfDeath === undefined) {
-      let year = new Date().getFullYear;
+      let year = new Date().getFullYear();
       personObject.age = year - personObject.yearOfBirth;
     } else {
       personObject.age = personObject.yearOfDeath - personObject.yearOfBirth;
     }
   }
-  console.log(arr);
+
+  function compare(personOne, personTwo) {
+    if (personOne.age > personTwo.age) return 1;
+    if (personOne.age == personTwo.age) return 0;
+    if (personOne.age < personTwo.age) return -1;
+  }
+
+  arr.sort(compare);
+
+  return arr[arr.length - 1];
 };
 
 // Do not edit below this line
