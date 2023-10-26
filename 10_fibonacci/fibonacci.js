030c9e92ee2aadd22529b3d6b5fba48e95c81354
Kenny Nguyen <sokenns@gmail.com>
2023-10-25 23:29:08 -0700

    Add solutions to 10th assessment

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  if (num < 1) {
+    return "OOPS";
+  }
+  arr = [1, 1];
+  for (let i = 2; i < num; i++) {
+    arr.push(arr[i - 1] + arr[i - 2]);
+  }
+  return arr[num - 1];
 };
 
 // Do not edit below this line
