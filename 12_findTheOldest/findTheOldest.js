a3ad3be3f236b40dfa2126dc51e98972220056d1
Kenny Nguyen <sokenns@gmail.com>
2023-10-26 01:09:03 -0700

    Add age to person's object properties

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+  for (let person in arr) {
+    let personObject = arr[person];
+    if (personObject.yearOfDeath === undefined) {
+      let year = new Date().getFullYear;
+      personObject.age = year - personObject.yearOfBirth;
+    } else {
+      personObject.age = personObject.yearOfDeath - personObject.yearOfBirth;
+    }
+  }
+  console.log(arr);
 };
 
 // Do not edit below this line
