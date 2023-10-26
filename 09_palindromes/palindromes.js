84df951b7386340769d9e5809f1d8808173a9cd2
Kenny Nguyen <sokenns@gmail.com>
2023-10-25 22:50:58 -0700

    Add solutions to 9th assessment

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let string = str.toLowerCase().replaceAll(/[^a-zA-Z0-9]/g,"");
+    let arr = string.split("");
+    for (i = 0; i < arr.length / 2; i++) {
+        let forwardChar = arr[i];
+        let backwardChar = arr[arr.length - (1 + i)];
+        if (forwardChar !== backwardChar) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
